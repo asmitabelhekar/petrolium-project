0696440d5044d96a85b248bb0d520342831f3fa6
DhananjayRaut <asmita.belhekar@gmail.com>
2020-02-21 17:34:17 +0530

    get api

--- src/app/pages/dataentryopening/dataentryopening.page.ts
+++ src/app/pages/dataentryopening/dataentryopening.page.ts
@@ -15,6 +15,7 @@ import { LoaderserviceService } from 'src/app/service/loader/loaderservice.servi
 export class DataentryopeningPage implements OnInit {
 
   today: any;
+  checkFuelType: any;
   previousOpeningBalance : any;
   openingModel: any = {}
   openingBalance: any;
@@ -22,6 +23,19 @@ export class DataentryopeningPage implements OnInit {
     { "type": "Petrol" },
     { "type": "Diesel" }
   ]
+
+  buttonsArray = [
+    {
+      "fuelType": "Petrol",
+      "indexFuel": "0",
+      "type": "0"
+    },
+    {
+      "fuelType": "Diesel",
+      "indexFuel": "1",
+      "type": "1"
+    }];
+
   constructor(
     public dataAdapter: DateAdapter<Date>,
     public location: Location,
@@ -37,6 +51,7 @@ export class DataentryopeningPage implements OnInit {
   }
 
   ngOnInit() {
+    this.openingModel['type'] = 2;
     this.openingModel['date'] = new Date().toJSON().split('T')[0];
     this.today = new Date().toJSON().split('T')[0];
   }
@@ -46,43 +61,62 @@ export class DataentryopeningPage implements OnInit {
   }
   openingBalanceSubmit() {
     this.loader.presentLoading();
-    let send_date = {};
-
-    send_date['date'] = this.openingModel['date'];
-    send_date['type'] = this.openingModel['type'];
-    send_date['balance'] = this.openingModel['openingbalance']
-
-    if (this.previousOpeningBalance == 0 || this.previousOpeningBalance == "") {
-
-      let url = environment.base_url + "balance";
-      this.apiCall.postWAu(url, send_date).subscribe(MyResponse => {
-        let msg = MyResponse['message'];
-        this.presentToast(msg);
-        this.router.navigate(['/dataentrycredit']);
-        this.loader.stopLoading();
-      }, error => {
-        this.loader.stopLoading();
-        this.presentToast("Something went wrong");
-        console.log(error.error.message);
-
-      })
-    } else {
-
-      let url = environment.base_url + "balance/" + this.openingModel['id'];
-      this.apiCall.put(url, send_date).subscribe(MyResponse => {
-        let msg = MyResponse['message'];
-        this.presentToast(msg);
-        this.router.navigate(['/dataentrycredit']);
-        this.loader.stopLoading();
-      }, error => {
-        this.loader.stopLoading();
-        this.presentToast("Something went wrong");
-        console.log(error.error.message);
-
-      })
+    if(this.openingModel['type'] == 2){
+      this.presentToast("Please select fuel type.");
     }
+    else{
 
+      let send_date = {};
 
+      send_date['date'] = this.openingModel['date'];
+      send_date['type'] = this.openingModel['type'];
+      send_date['balance'] = this.openingModel['openingbalance']
+  
+      if (this.previousOpeningBalance == 0 || this.previousOpeningBalance == "") {
+  
+        let url = environment.base_url + "balance";
+        this.apiCall.postWAu(url, send_date).subscribe(MyResponse => {
+          let msg = MyResponse['message'];
+          this.presentToast(msg);
+          // this.router.navigate(['/dataentrycredit']);
+          this.loader.stopLoading();
+        }, error => {
+          this.loader.stopLoading();
+          this.presentToast("Something went wrong");
+          console.log(error.error.message);
+  
+        })
+      } else {
+  
+        let url = environment.base_url + "balance/" + this.openingModel['id'];
+        this.apiCall.put(url, send_date).subscribe(MyResponse => {
+          let msg = MyResponse['message'];
+          this.presentToast(msg);
+          // this.router.navigate(['/dataentrycredit']);
+          this.loader.stopLoading();
+        }, error => {
+          this.loader.stopLoading();
+          this.presentToast("Something went wrong");
+          console.log(error.error.message);
+  
+        })
+      }
+  
+    }
+  
+
+  }
+
+  fuelTypeCheck(fuelType) {
+    this.checkFuelType = fuelType;
+    if (fuelType == 0) {
+
+      this.openingModel['type'] = 0;
+      this.getOpeningBalance();
+    } else {
+      this.openingModel['type'] = 1;
+      this.getOpeningBalance();
+    }
   }
 
   async presentToast(message) {
@@ -109,34 +143,36 @@ export class DataentryopeningPage implements OnInit {
   getOpeningBalance() {
     this.loader.presentLoading();
 
-    let objectt = {};
-    objectt['date'] = this.openingModel['date'];
-    objectt['type'] = this.openingModel['type'];
-    let url = environment.base_url + "balance?" + "filters=" + JSON.stringify(objectt);
-    console.log("url :" + url);
-    this.apiCall.get(url).subscribe(MyResponse => {
-
-      let getData = MyResponse['result']['list'];
-      if (getData.length > 0) {
-        for (let i = 0; i < getData.length; i++) {
-          this.openingModel['id'] = MyResponse['result']['list'][i]['id'];
-          this.previousOpeningBalance = MyResponse['result']['list'][i]['balance'];
-          this.openingModel['openingbalance'] = MyResponse['result']['list'][i]['balance'];
+      let objectt = {};
+      objectt['date'] = this.openingModel['date'];
+      objectt['type'] = this.openingModel['type'];
+      let url = environment.base_url + "balance?" + "filters=" + JSON.stringify(objectt);
+      console.log("url :" + url);
+      this.apiCall.get(url).subscribe(MyResponse => {
+  
+        let getData = MyResponse['result']['list'];
+        if (getData.length > 0) {
+          for (let i = 0; i < getData.length; i++) {
+            this.openingModel['id'] = MyResponse['result']['list'][i]['id'];
+            this.previousOpeningBalance = MyResponse['result']['list'][i]['balance'];
+            this.openingModel['openingbalance'] = MyResponse['result']['list'][i]['balance'];
+          }
+  
+        } else {
+          this.openingModel['id'] = "";
+          this.previousOpeningBalance = 0;
+          this.openingModel['openingbalance'] = 0;
         }
-
-      } else {
-        this.openingModel['id'] = "";
-        this.previousOpeningBalance = 0;
-        this.openingModel['openingbalance'] = 0;
-      }
-
-
-      this.loader.stopLoading();
-    },
-      error => {
+  
+  
         this.loader.stopLoading();
-        this.presentToast("Something went wrong");
-
-      })
-  }
+      },
+        error => {
+          this.loader.stopLoading();
+          this.presentToast("Something went wrong");
+  
+        })
+    }
+  
+  
 }
--- src/app/pages/petroldieselamount/petroldieselamount.page.ts
+++ src/app/pages/petroldieselamount/petroldieselamount.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { LoaderserviceService } from 'src/app/service/loader/loaderservice.service';
+import { ApicallService } from 'src/app/service/apicall/apicall.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-petroldieselamount',
@@ -9,12 +12,33 @@ import { ToastController } from '@ionic/angular';
 export class PetroldieselamountPage implements OnInit {
 
   userModel: any = {};
-  constructor(public toasController: ToastController) { }
+  checkStatus: any;
+  url = "http://www.mocky.io/v2/5e4fc0663000009000226b53";
+// url = "http://www.mocky.io/v2/5e4fc5ef3000005100226b7f";
+  constructor(
+    public toasController: ToastController,
+    public loader: LoaderserviceService,
+    public apiCall: ApicallService) { }
 
   ngOnInit() {
+    this.getPetrolDieselAmount();
+    if (this.userModel['petrolamount'] != null || this.userModel['petrolamount'] != "" || this.userModel['petrolamount'] != undefined ||
+      this.userModel['dieselamount'] != null || this.userModel['dieselamount'] != "" || this.userModel['dieselamount'] != undefined) {
+      this.checkStatus = "update";
+    }
+    else {
+      this.checkStatus = "add";
+    }
   }
 
   submit() {
+    if (this.checkStatus == "add") {
+      alert("post");
+    } else if (this.checkStatus == "update") {
+      alert("put");
+    } else {
+
+    }
     this.presentToast("Amount submited")
   }
 
@@ -24,5 +48,61 @@ export class PetroldieselamountPage implements OnInit {
       duration: 4000
     });
     toast.present();
+  }
+
+  getPetrolDieselAmount() {
+    this.loader.presentLoading();
+    this.apiCall.get(this.url).subscribe(MyResponse => {
+      let getData = MyResponse[0];
+      this.userModel['petrolamount'] = getData['petrolamount'];
+      this.userModel['dieselamount'] = getData['dieselamount'];
+      this.loader.stopLoading();
+    },
+      error => {
+        alert("display data:" + error);
+        this.loader.stopLoading();
+        this.presentToast("Something went wrong");
+      })
+  }
+
+  fuelTypeBalanceSubmit() {
+    this.loader.presentLoading();
+  
+      let send_date = {};
+
+      send_date['petrolamount'] = this.userModel['petrolamount'];
+      send_date['dieselamount'] = this.userModel['dieselamount'];
+  
+      if (this.checkStatus == "add") {
+  
+        let url = environment.base_url + "balance";
+        this.apiCall.postWAu(url, send_date).subscribe(MyResponse => {
+          let msg = MyResponse['message'];
+          this.presentToast(msg);
+          this.loader.stopLoading();
+        }, error => {
+          this.loader.stopLoading();
+          this.presentToast("Something went wrong");
+          console.log(error.error.message);
+  
+        })
+      } else {
+  
+        let url = environment.base_url + "balance/" + this.userModel['id'];
+        this.apiCall.put(url, send_date).subscribe(MyResponse => {
+          let msg = MyResponse['message'];
+          this.presentToast(msg);
+          this.loader.stopLoading();
+        }, error => {
+          this.loader.stopLoading();
+          this.presentToast("Something went wrong");
+          console.log(error.error.message);
+  
+        })
+      }
+  
+    
+  
+
   }
 }
